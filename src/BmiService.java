5b868aad6d0cd97e14c8c1c20d44b089084fdaf5
Xenia <xzinnatullina@mail.ru>
2023-06-13 23:10:16 +0300

    scr.changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,9 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        double h = Math.pow(1.73,2);
-        double index = weight/ h;
+        int a = 2;
+        double h = Math.pow(height, a);
+        double index = weight / h;
 
         return (int) index;
     }
-
-
 }
