700b781ed70b2a0e2c08c2b47075b1b08dc20b5d
Xenia <xzinnatullina@mail.ru>
2023-06-13 23:23:48 +0300

    scr.modified

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 51;
-        double height = 1.73;
+        int weight;
+        double height;
         int index = (int) service.calculate(51, 1.73);
         System.out.println(index);
 
